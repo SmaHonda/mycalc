79254df039f9ea7922475525e8aad9ba9db8cfcd
SmaHonda <86571238+SmaHonda@users.noreply.github.com>
2021-09-12 16:52:00 +0800

    0912

---
+++ javascript.js
@@ -0,0 +1,8 @@
+$('#count').click(function(){
+    $('#root').css('display','block');
+    $('#root2').css('display','none');
+})
+$('#count878').click(function(){
+    $('#root').css('display','none');
+    $('#root2').css('display','block');
+})
